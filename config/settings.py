3ed3b2dc693b4df01d1515057025d0e1e4ed382b
HAMA Barhamou <hamabarhamou@gmail.com>
2025-02-01 19:10:06 +0100

    ajour de https://setes.onrender.com/ dans ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -165,7 +165,7 @@ else:
     #ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default='setes.onrender.com')
     # settings.py
     ALLOWED_HOSTS = [
-        'https://www.setes.onrender.com',
+        'https://setes.onrender.com/',
         'localhost',
         '127.0.0.1'
     ]
